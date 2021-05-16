f50fa8edc66efafaa54f63398218a6df18d31021
Douglas Hill <hello@douglashill.co>
2021-05-16 15:53:59 +0100

    Fix attempting to save untitled documents

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,6 +1,9 @@
 exports.activate = function() {
     nova.workspace.onDidAddTextEditor((openedEditor) => {
         openedEditor.onDidStopChanging((changedEditor) => {
+            if (changedEditor.document.isUntitled) {
+                return;
+            }
             changedEditor.save(); 
         });
     });
