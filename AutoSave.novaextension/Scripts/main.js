9e0340f445371bf0476953fcf57e8f26b5fbd27b
Douglas Hill <hello@douglashill.co>
2021-10-23 19:20:06 +0100

    Save with 3 second delay

--- AutoSave.novaextension/Scripts/main.js
+++ AutoSave.novaextension/Scripts/main.js
@@ -1,10 +1,27 @@
 exports.activate = function() {
     nova.workspace.onDidAddTextEditor((openedEditor) => {
-        openedEditor.onDidStopChanging((changedEditor) => {
+        openedEditor.onDidChange((changedEditor) => {
             if (changedEditor.document.isUntitled) {
+                console.log("Document is untitled. Not saving.")
                 return;
             }
-            changedEditor.save(); 
+
+            if (changedEditor.dh_activeSaveTimerID !== undefined) {
+                console.log("Clearing timer " + changedEditor.dh_activeSaveTimerID + " in editor " + changedEditor.document.uri);
+                clearTimeout(changedEditor.dh_activeSaveTimerID);
+            } else {
+                console.log("Not clearing timer because it is " + changedEditor.dh_activeSaveTimerID + " in editor " + changedEditor.document.uri);
+            }
+
+            changedEditor.dh_activeSaveTimerID = setTimeout(function(){
+                console.log("Save frm timer " + changedEditor.dh_activeSaveTimerID + " in editor " + changedEditor.document.uri);
+                changedEditor.dh_activeSaveTimerID = undefined
+                changedEditor.save();
+            }, 3000);
+            console.log("Setting  timer " + changedEditor.dh_activeSaveTimerID + " in editor " + changedEditor.document.uri);
+        });
+        openedEditor.onDidDestroy((destroyedEditor) => {
+            console.log("Destroyed editor " + destroyedEditor.document.uri);
         });
     });
 }
